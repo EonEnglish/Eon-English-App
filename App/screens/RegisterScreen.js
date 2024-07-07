21dff849e72471e95f562f78640d3028daa3c450
Jessenia Chan <jcholly4@gmail.com>
2024-07-07 15:16:33 -0700

    new register screen

--- App/screens/RegisterScreen.js
+++ App/screens/RegisterScreen.js
@@ -1,38 +1,24 @@
+import React, { useState } from 'react';
 import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import React, { useEffect, useState } from 'react';
 import { auth } from "../firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { useNavigation } from '@react-navigation/native';
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
-const LoginScreen = ({ navigation }) => {
-    const[email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
+const RegisterScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            if(user) {
-                navigation.replace("Tab")
-            }
-        })
-
-        return unsubscribe;
-    }, [])
-
-
-    const handleLogIn = () => {
-        signInWithEmailAndPassword(auth, email, password)
+    const handleSignUp = () => {
+        createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            // Signed up 
             const user = userCredential.user;
-            console.log("Logged in with: " + user.email);
+            console.log("Registered with: " + user.email);
+            navigation.replace("Tab"); 
         })
         .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode)
-            console.log(errorMessage);
+            console.log(error.code);
+            console.log(error.message);
         });
-    }
+    };
 
     return (
         <KeyboardAvoidingView
@@ -40,13 +26,13 @@ const LoginScreen = ({ navigation }) => {
             behavior='padding'
         >
             <View style={styles.inputContainer}>
-                <TextInput 
+                <TextInput
                     placeholder='Email'
                     value={email}
                     onChangeText={text => setEmail(text)}
                     style={styles.input}
                 />
-                <TextInput 
+                <TextInput
                     placeholder='Password'
                     value={password}
                     onChangeText={text => setPassword(text)}
@@ -56,24 +42,18 @@ const LoginScreen = ({ navigation }) => {
             </View>
 
             <View style={styles.buttonContainer}>
-                <TouchableOpacity 
-                    onPress={handleLogIn}
+                <TouchableOpacity
+                    onPress={handleSignUp}
                     style={styles.button}
                 >
-                    <Text style={styles.buttonText}>Login</Text>
-                </TouchableOpacity>
-                <TouchableOpacity 
-                    onPress={() => navigation.navigate("Register")}
-                    style={[styles.button, styles.buttonOutline]}
-                >
-                    <Text style={styles.buttonOutlineText}>Register</Text>
+                    <Text style={styles.buttonText}>Register</Text>
                 </TouchableOpacity>
             </View>
         </KeyboardAvoidingView>
-    )
-}
+    );
+};
 
-export default LoginScreen
+export default RegisterScreen;
 
 const styles = StyleSheet.create({
     container: {
@@ -104,20 +84,9 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         alignItems: 'center',
     },
-    buttonOutline: {
-        backgroundColor: 'white',
-        marginTop: 5,
-        borderColor: '#0782F9',
-        borderWidth: 2,
-    },
     buttonText: {
         color: 'white',
         fontWeight: '700',
         fontSize: 16,
     },
-    buttonOutlineText: {
-        color: '#0782F9',
-        fontWeight: '700',
-        fontSize: 16,
-    },
-})
+});
