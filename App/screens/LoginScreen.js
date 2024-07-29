85c78961e00fb3732f0cdbb7ea02469156af5ecf
J12aGuan <96441175+J12aGuan@users.noreply.github.com>
2024-07-28 21:10:16 -0700

    commit

--- App/screens/LoginScreen.js
+++ App/screens/LoginScreen.js
@@ -159,7 +159,7 @@ const styles = StyleSheet.create({
         width: '80%',
         justifyContent: 'center',
         alignItems: 'center',
-        marginTop: 30,
+        marginTop: 25,
     },
     loginButton: {
         backgroundColor: '#0782F9',
