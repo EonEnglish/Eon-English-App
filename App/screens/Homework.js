81672490f4b74e5ba4477925bc3d6e93a348bd94
J12aGuan <96441175+J12aGuan@users.noreply.github.com>
2024-08-31 01:14:04 -0700

    commit

--- App/screens/Homework.js
+++ App/screens/Homework.js
@@ -8,20 +8,20 @@ import { db } from "../firebase";
 const Homework = ({ navigation }) => {
   const [lessonsState, setLessonsState] = useState([]);
   const lessons = [
-    { id: '1', title: 'Where are you going?', status: 'NOT STARTED', color: '#0782F9' },
-    { id: '2', title: 'Time for School', status: 'NOT STARTED', color: '#8D56FF' },
-    { id: '3', title: 'Sports', status: 'NOT STARTED', color: '#8D56FF' },
+    { id: '1', title: 'Where are you going?', status: 'NOT STARTED', color: '#F9C407' },
+    { id: '2', title: 'Time for School', status: 'NOT STARTED', color: '#F9C407' },
+    { id: '3', title: 'Sports', status: 'NOT STARTED', color: '#F9C407' },
     { id: '4', title: 'Music and Art', status: 'NOT STARTED', color: '#F9C407' },
     { id: '5', title: 'Home', status: 'NOT STARTED', color: '#F9C407' },
-    { id: '6', title: 'Meals', status: 'NOT STARTED', color: '#0782F9' },
-    { id: '7', title: 'Fast Food', status: 'NOT STARTED', color: '#0782F9' },
+    { id: '6', title: 'Meals', status: 'NOT STARTED', color: '#F9C407' },
+    { id: '7', title: 'Fast Food', status: 'NOT STARTED', color: '#F9C407' },
     { id: '8', title: 'Review', status: 'NOT STARTED', color: '#F9C407' },
-    { id: '9', title: 'Nature', status: 'NOT STARTED', color: '#8D56FF' },
-    { id: '10', title: 'Animals', status: 'NOT STARTED', color: '#0782F9' },
+    { id: '9', title: 'Nature', status: 'NOT STARTED', color: '#F9C407' },
+    { id: '10', title: 'Animals', status: 'NOT STARTED', color: '#F9C407' },
     { id: '11', title: 'Shopping', status: 'NOT STARTED', color: '#F9C407' },
-    { id: '12', title: 'Birthdays', status: 'NOT STARTED', color: '#8D56FF' },
-    { id: '13', title: 'Holidays', status: 'NOT STARTED', color: '#8D56FF' },
-    { id: '14', title: 'Review', status: 'NOT STARTED', color: '#0782F9' },
+    { id: '12', title: 'Birthdays', status: 'NOT STARTED', color: '#F9C407' },
+    { id: '13', title: 'Holidays', status: 'NOT STARTED', color: '#F9C407' },
+    { id: '14', title: 'Review', status: 'NOT STARTED', color: '#F9C407' },
     // Add more lessons as needed
   ];
 
@@ -53,27 +53,27 @@ const Homework = ({ navigation }) => {
           let vocabMatchPhotoExists = vocabMatchPhotoDoc.exists();
           let fillInTheBlankExists = fillInTheBlankDoc.exists();
 
-          if (vocabMatchExists || vocabMatchPhotoExists || fillInTheBlankExists) {
-            newLessonsState[i - 1].status = 'IN PROGRESS';
-          }
-
           if (vocabMatchExists && vocabMatchPhotoExists && fillInTheBlankExists) {
             newLessonsState[i - 1].status = 'COMPLETED';
+            newLessonsState[i - 1].color = '#0782F9';
+          }
+          else if (vocabMatchExists || vocabMatchPhotoExists || fillInTheBlankExists) {
+            newLessonsState[i - 1].status = 'IN PROGRESS';
+            newLessonsState[i - 1].color = '#8D56FF';
           }
 
         } catch (error) {
           consol
           e.error('Error fetching homework data:', error);
         }
-
-        setLessonsState(newLessonsState);
       }
+      setLessonsState(newLessonsState);
     };
 
     if (isFocused) {
       fetchData(); // Fetch data when the screen is focused
     }
-  }, [isFocused, navigation]);
+  }, [navigation]);
 
   const renderLessonItem = ({ item }) => (
     <TouchableOpacity
@@ -92,6 +92,10 @@ const Homework = ({ navigation }) => {
   const dimensions = Dimensions.get('window');
   const itemWidth = (dimensions.width / numColumns) - 20; // Adjusting for margin
 
+  if(lessonsState < 14) {
+    return <Text>Loading...</Text>;
+  }
+  
   return (
     <View style={styles.container}>
       <FlatList
