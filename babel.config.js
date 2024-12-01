37a01ffa78a7d3e47d2725d797b802b65c5861b3
Alex Ischuk <qwertygo85@gmail.com>
2024-12-01 22:32:01 +0200

    feat(134): added a module to load .env variables

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        // if your env variables aren't updating,
+        // start expo with `--clear` option to clean cache
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          allowUndefined: false,
+        },
+      ],
+    ],
   };
 };
---
+++ src/lib/configs/firebase.config.js
@@ -0,0 +1,19 @@
+import {
+  FIREBASE_PROJECT_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_APP_ID,
+  FIREBASE_API_KEY,
+  FIREBASE_MESSAGING_SENDER_ID,
+} from "@env";
+
+export const GetFirebaseConfig = () => {
+  return {
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+  };
+}
