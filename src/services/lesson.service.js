d8b4fd5a6129d34fe7d47da142d0bf10b03215a4
Alex Ischuk <qwertygo85@gmail.com>
2024-12-30 12:34:59 +0200

    feat: added completation of lesson. saving data in user profile

--- src/services/lesson.service.js
+++ src/services/lesson.service.js
@@ -6,10 +6,11 @@ import {
   orderBy,
   query,
   setDoc,
+  updateDoc,
   where,
 } from "@firebase/firestore";
-import { CollectionNames } from "../lib/constant";
-import { db } from "./firebase";
+import { CollectionNames, VocabEnum } from "../lib/constant";
+import { auth, db } from "./firebase";
 
 /**
  *
@@ -93,10 +94,14 @@ export const GetLessonCompletion = async (userId, lessonId) => {
    */
   const userAttempts = userProgress.passing_attempts || [];
 
-  const vocabDone = userAttempts.some((attempt) => attempt.type === "vocab");
-  const imageDone = userAttempts.some((attempt) => attempt.type === "image");
+  const vocabDone = userAttempts.some(
+    (attempt) => attempt.type === VocabEnum.VOCAB,
+  );
+  const imageDone = userAttempts.some(
+    (attempt) => attempt.type === VocabEnum.IMAGE,
+  );
   const fillBlankDone = userAttempts.some(
-    (attempt) => attempt.type === "fill-blank",
+    (attempt) => attempt.type === VocabEnum.FILL_BLANK,
   );
 
   return {
@@ -135,12 +140,97 @@ export const CompleteHomework = async (
         passing_attempts: [],
       };
 
-  progressData.passing_attempts.push({
+  if (!progressDoc.exists()) {
+    progressData.passing_attempts.push({
+      completed_time: new Date(),
+      total_score: totalScore,
+      type,
+      score,
+    });
+
+    await setDoc(userProgressRef, progressData);
+
+    // add the lesson to started if the progress didn't exist
+    await AddLessonToStarted(auth.currentUser.uid, lessonId);
+    return;
+  }
+
+  let attempt = {
     completed_time: new Date(),
     total_score: totalScore,
     type,
     score,
-  });
+  };
+
+  const attemptIndex = progressData.passing_attempts.findIndex(
+    (item) => item.type === type,
+  );
+  if (attemptIndex > -1) {
+    attempt = progressData.passing_attempts[attemptIndex];
+    // delete item by index
+    progressData.passing_attempts.splice(attemptIndex, 1);
+  }
+
+  if (score > attempt.score) {
+    attempt.score = score;
+    attempt.total_score = totalScore;
+    attempt.completed_time = new Date();
+  }
+
+  progressData.passing_attempts.push(attempt);
 
   await setDoc(userProgressRef, progressData);
+
+  // TODO: consider to make it as background job
+  const statusOfLesson = await GetLessonCompletion(
+    auth.currentUser.uid,
+    lessonId,
+  );
+
+  // Add the lesson to done if the user has completed all categories
+  if (
+    statusOfLesson.fill_blank_done &&
+    statusOfLesson.image_done &&
+    statusOfLesson.vocab_done
+  ) {
+    await AddLessonToDone(auth.currentUser.uid, lessonId);
+  }
+};
+
+/**
+ *
+ * @param {string} userId
+ * @param {string} lessonId
+ */
+const AddLessonToStarted = async (userId, lessonId) => {
+  const userRef = doc(db, CollectionNames.USER, userId);
+  const userData = await getDoc(userRef);
+  const userPlain = userData.exists()
+    ? userData.data()
+    : { done_lesson_ids: [], started_lesson_ids: [] };
+
+  const setLessonIds = new Set(Array.from(userPlain.started_lesson_ids));
+  setLessonIds.add(lessonId);
+
+  userPlain.started_lesson_ids = Array.from(setLessonIds);
+  await updateDoc(userRef, userPlain);
+};
+
+/**
+ *
+ * @param {string} userId
+ * @param {string} lessonId
+ */
+const AddLessonToDone = async (userId, lessonId) => {
+  const userRef = doc(db, CollectionNames.USER, userId);
+  const userData = await getDoc(userRef);
+  const userPlain = userData.exists()
+    ? userData.data()
+    : { done_lesson_ids: [], started_lesson_ids: [] };
+
+  const setLessonIds = new Set(Array.from(userPlain.done_lesson_ids));
+  setLessonIds.add(lessonId);
+
+  userPlain.done_lesson_ids = Array.from(setLessonIds);
+  await updateDoc(userRef, userPlain);
 };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -5,7 +5,6 @@ import { auth, db } from "../services/firebase";
 
 export const RegisterUser = async ({ email, password }) => {
   await createUserWithEmailAndPassword(auth, email, password);
-  auth.currentUser.uid;
   const userRef = doc(db, CollectionNames.USER, auth.currentUser.uid);
   await setDoc(userRef, { done_lesson_ids: [], started_lesson_ids: [] });
 };
