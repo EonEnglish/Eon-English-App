ed6e5181b5f643c633f03470e9fd327e592e9347
MenlyCSE <MenlyCSE@gmail.com>
2025-01-10 20:06:03 -0500

    staging version update
    - Does not affect functionality

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { bundler: 'metro' }]
-    ],
+    presets: [["babel-preset-expo", { bundler: "metro" }]],
     plugins: [
       [
         // if your env variables aren't updating,
